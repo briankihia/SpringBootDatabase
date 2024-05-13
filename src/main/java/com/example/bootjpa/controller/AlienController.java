3b6804a4e054c2f228088aa3f684fbe445fa6ce5
brian <brianjohnkiama@gmail.com>
2024-05-14 02:34:45 +0300

    client being able to request for data and displaying it in a template for view

--- src/main/java/com/example/bootjpa/controller/AlienController.java
+++ src/main/java/com/example/bootjpa/controller/AlienController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.example.bootjpa.model.Alien;
 import com.example.bootjpa.dao.AlienRepo;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AlienController
@@ -20,11 +22,32 @@ public class AlienController
     }
 
 //    we create a controller that will accept addAlien
+//    below is used to send data to the database
     @RequestMapping("/addAlien")
     public String addAlien(Alien alien)
     {
         repo.save(alien);
         return "home.jsp";
+    }
+
+//    this is the controller for get alien
+//    we don't want to get alien we just want the id
+//    we will also use requestParam which is used to get data from client
+//    we use ModelAndView because we are getting data and using it not sending to database
+    @RequestMapping("/getAlien")
+    public ModelAndView getAlien(@RequestParam int aid)
+    {
+//        below showAlien.jsp is a view name put where a constructor should be
+//        the below ModelAndView doesn't contain anything at first, it's just use to store data
+        ModelAndView mv = new ModelAndView("showAlien.jsp");
+//        below orElse art is used to remove an error that you get without it
+//        and incase client is trying to get data that is not in the database it will return null values
+        Alien alien =repo.findById(aid).orElse(new Alien());
+//        above we put some data in alien and then we use ModelAndView and put data content of alien
+//        after writing below, we can now access the data in our template but using alien
+        mv.addObject(alien);
+//        when you use mv you just return mv
+        return mv;
     }
 
 }
--- src/main/webapp/home.jsp
+++ src/main/webapp/home.jsp
@@ -11,10 +11,22 @@
     <form action="addAlien">
         <input type="text" name="aid"><br>
         <input type="text" name="aname"><br>
+        <input type="text" name="tech"><br>
         <input type="submit"><br>
-
     </form>
+
+    <!--now we are going to fetch the alien based on the aid-->
+    <!--thus we create a form and a controller for that-->
+
+        <form action="getAlien">
+            <input type="text" name="aid"><br>
+            <input type="submit"><br>
+        </form>
 
 
 </body>
 </html>
+
+<!---->
+<!--why do we need a service layout? As a controller , we are always not sure if data is coming from database.Sometimes data comes from the network, user.Thus we need to process something, that processing is done in the service layout-->
+<!--if data is coming from the repository then service layer will react with the repository-->
